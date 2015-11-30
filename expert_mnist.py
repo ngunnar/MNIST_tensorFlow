6aa8cfff5b55515a8411423978a2ef03b649b945
Niklas <gunnarsson_niklas@hotmail.com>
2015-11-30 23:57:02 +0100

    use interactive session

--- expert_mnist.py
+++ expert_mnist.py
@@ -1,7 +1,6 @@
 import input_data
 import tensorflow as tf
 
-
 def weight_variable(shape):
     initial = tf.truncated_normal(shape, stddev=0.1)
     return tf.Variable(initial)
@@ -17,12 +16,11 @@ def max_pool_2x2(x):
     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 
-
 print 'Loading data...'
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 print 'Loading data done!'
 
-sess = tf.Session()
+sess = tf.InteractiveSession()
 
 """Data set"""
 print
@@ -48,13 +46,11 @@ x_image = tf.reshape(x, [-1,28,28,1])
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
 
-
 W_conv2 = weight_variable([5, 5, 32, 64])
 b_conv2 = bias_variable([64])
 
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 h_pool2 = max_pool_2x2(h_conv2)
-
 
 W_fc1 = weight_variable([7 * 7 * 64, 1024])
 b_fc1 = bias_variable([1024])
@@ -64,7 +60,6 @@ h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
 keep_prob = tf.placeholder("float")
 h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-
 
 W_fc2 = weight_variable([1024, 10])
 b_fc2 = bias_variable([10])
@@ -80,8 +75,8 @@ sess.run(tf.initialize_all_variables())
 for i in range(20000):
     batch_xs, batch_ys = mnist.train.next_batch(50)
     if i%100 == 0:
-        train_accuracy = accuracy.eval(session=sess,feed_dict={x:batch_xs, y_: batch_ys, keep_prob: 1.0})
+        train_accuracy = accuracy.eval(feed_dict={x:batch_xs, y_: batch_ys, keep_prob: 1.0})
         print "step %d, training accuracy %g"%(i, train_accuracy)
-    train_step.run(session=sess,feed_dict={x: batch_xs, y_: batch_ys, keep_prob: 0.5})
+    train_step.run(feed_dict={x: batch_xs, y_: batch_ys, keep_prob: 0.5})
 
-print "test accuracy %g"%accuracy.eval(session=sess,feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0})
+print "test accuracy %g"%accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0})
